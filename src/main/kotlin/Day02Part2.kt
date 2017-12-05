4be769ec76bf05f038e3191925a6937015795d98
Andy Bell <andyrbell@gmail.com>
2017-12-05 01:08:57 +0000

    Day 2 part 2

--- src/main/kotlin/Day02Part2.kt
+++ src/main/kotlin/Day02Part2.kt
@@ -3,8 +3,16 @@ import Utils.toInts
 class Day02Part2 {
     fun checksum(line: String): Int {
 
-        // WIP
-        return 0
+        val fields = toInts(line).sorted().reversed()
+        val combinations = fields.flatMap { field -> fields.map { Pair(field, it) } }
+        println(combinations)
+
+        return combinations
+                .filter { it.first > it.second }
+                .map { (first, second) -> when (first % second) {
+                    0 -> first / second
+                    else -> 0
+                } }.first { it > 0 }
     }
 
     fun checksum(lines: List<String>): Int = lines.map { checksum(it) }.sum()
