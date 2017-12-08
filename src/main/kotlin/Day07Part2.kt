72448cfe7a0e86d18e48decd147103a2bd766578
Andy Bell <andyrbell@gmail.com>
2017-12-08 00:50:23 +0000

    Day 7 part 2

--- src/main/kotlin/Day07Part2.kt
+++ src/main/kotlin/Day07Part2.kt
@@ -1,21 +1,45 @@
 class Day07Part2 {
     fun solve(input: List<String>): Int {
-        val referenceCount = mutableMapOf<String, Int>()
-        val register = input.map { parseNode(it, referenceCount) }.associateBy { it.name }
+        val treeNodesByName = input.map { parseNode(it) }.associateBy { it.name }
 
-        val rootNode = referenceCount.entries.first { it.value == 1 }.key
-
-        val calculatedWeight = calculateWeight(register[rootNode]!!, register)
-        println("rootNode weight: $calculatedWeight")
-
-        fun findNode(name: String): TreeNode {
-            return register[name]!!
+        treeNodesByName.forEach { _, parentNode ->
+            parentNode.childNames.forEach {
+                childName ->
+                val childNode = treeNodesByName[childName]!!
+                parentNode.childNodes.add(childNode)
+                childNode.parentNode = parentNode
+            }
         }
+        val rootNode = treeNodesByName.entries.first { it.value.parentNode == null }.key
 
-        return 0
+
+        return findNode(treeNodesByName[rootNode]!!, 0)
+
     }
 
-    private fun parseNode(line: String, referenceCount: MutableMap<String, Int>): TreeNode {
+    private fun findNode(node: TreeNode, difference: Int): Int {
+
+        return if (node.isBalanced()) {
+            node.weight - difference
+        } else {
+
+            val unbalancedTree = node.childNodes.groupBy { it.inherentWeight() }
+                    .minBy { it.value.size }!!
+                    .value
+                    .first()
+
+            val diff = node.childNodes
+                    .groupBy { it.inherentWeight() }
+                    .keys
+                    .distinct()
+                    .reduce { x, y -> Math.abs(x - y) }
+            findNode(unbalancedTree, diff)
+        }
+
+
+    }
+
+    private fun parseNode(line: String): TreeNode {
         val regex = Regex("""(.*) \((\d+)\)( -> (.*))?""")
         val matchResult = regex.matchEntire(line)
         return when (matchResult != null) {
@@ -23,38 +47,20 @@ class Day07Part2 {
                 val name = matchResult!!.groups[1]!!.value
                 val weight = Integer.valueOf(matchResult.groups[2]!!.value)
                 val childNames = matchResult.groups[4]?.value
-                referenceCount.put(name, referenceCount.getOrDefault(name, 0) + 1)
 
                 val children = childNames?.split(""",\s+""".toRegex())?.toList()
-                children?.forEach {
-                    referenceCount.put(it, referenceCount.getOrDefault(it, 0) + 1)
-                }
                 TreeNode(name, weight, children.orEmpty())
             }
             else -> throw Exception("Invalid line: $line")
         }
     }
 
-    private fun calculateWeight(nodes: List<TreeNode>, register: Map<String, TreeNode>): Int {
-        return nodes.map { calculateWeight(it, register) }.sum()
-    }
-
-    private fun calculateWeight(node: TreeNode, register: Map<String, TreeNode>): Int {
-        return when (node.hasChildren()) {
-            false -> node.weight
-            true -> node.weight + calculateWeight(node.children.mapNotNull { register[it] }.toList(), register)
-        }
-    }
-
-    fun isBalanced(node: TreeNode, register: Map<String, TreeNode>): Boolean {
-        return when (node.hasChildren()) {
-            false -> true
-            true -> node.children.mapNotNull { register[it] }.map { calculateWeight(it, register) }.distinct().size == 1
-        }
-    }
-
-    data class TreeNode(val name: String, val weight: Int, val children: List<String>) {
-        fun hasChildren(): Boolean = children.isNotEmpty()
+    data class TreeNode(val name: String, val weight: Int, val childNames: List<String>,
+                        val childNodes: MutableList<TreeNode> = mutableListOf(),
+                        var parentNode: TreeNode? = null) {
+        fun hasChildren(): Boolean = childNames.isNotEmpty()
+        fun inherentWeight(): Int = weight + childNodes.map { it.inherentWeight() }.sum()
+        fun isBalanced(): Boolean = childNodes.map { it.inherentWeight() }.distinct().size == 1
     }
 
 }
