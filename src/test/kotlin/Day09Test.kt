e73f713d136e3097f0493d452a77426836a2c287
Andy Bell <andyrbell@gmail.com>
2017-12-10 00:18:53 +0000

    Use assertj

--- src/test/kotlin/Day09Test.kt
+++ src/test/kotlin/Day09Test.kt
@@ -1,42 +1,40 @@
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-import kotlin.test.assertEquals
 
 class Day09Test {
     @Test
     fun part1() {
-        assertEquals(1, Day09Part1().solve("{}"))
-        assertEquals(6, Day09Part1().solve("{{{}}}"))
-        assertEquals(5, Day09Part1().solve("{{},{}}"))
-        assertEquals(16, Day09Part1().solve("{{{},{},{{}}}}"))
-        assertEquals(1, Day09Part1().solve("{<a>,<a>,<a>,<a>}"))
-        assertEquals(9, Day09Part1().solve("{{<ab>},{<ab>},{<ab>},{<ab>}}"))
-        assertEquals(9, Day09Part1().solve("{{<!!>},{<!!>},{<!!>},{<!!>}}"))
-        assertEquals(3, Day09Part1().solve("{{<a!>},{<a!>},{<a!>},{<ab>}}"))
+        assertThat(Day09Part1().solve("{}")).isEqualTo(1)
+        assertThat(Day09Part1().solve("{{{}}}")).isEqualTo(6)
+        assertThat(Day09Part1().solve("{{},{}}")).isEqualTo(5)
+        assertThat(Day09Part1().solve("{{{},{},{{}}}}")).isEqualTo(16)
+        assertThat(Day09Part1().solve("{<a>,<a>,<a>,<a>}")).isEqualTo(1)
+        assertThat(Day09Part1().solve("{{<ab>},{<ab>},{<ab>},{<ab>}}")).isEqualTo(9)
+        assertThat(Day09Part1().solve("{{<!!>},{<!!>},{<!!>},{<!!>}}")).isEqualTo(9)
+        assertThat(Day09Part1().solve("{{<a!>},{<a!>},{<a!>},{<ab>}}")).isEqualTo(3)
     }
 
     @Test
     fun part2() {
-        assertEquals(0, Day09Part2().solve("<>"))
-        assertEquals(17, Day09Part2().solve("<random characters>"))
-        assertEquals(3, Day09Part2().solve("<<<<>"))
-        assertEquals(2, Day09Part2().solve("<{!>}>"))
-        assertEquals(0, Day09Part2().solve("<!!>"))
-        assertEquals(0, Day09Part2().solve("<!!!>>"))
-        assertEquals(10, Day09Part2().solve("<{o\"i!a,<{i<a>"))
+        assertThat(Day09Part2().solve("<>")).isEqualTo(0)
+        assertThat(Day09Part2().solve("<random characters>")).isEqualTo(17)
+        assertThat(Day09Part2().solve("<<<<>")).isEqualTo(3)
+        assertThat(Day09Part2().solve("<{!>}>")).isEqualTo(2)
+        assertThat(Day09Part2().solve("<!!>")).isEqualTo(0)
+        assertThat(Day09Part2().solve("<!!!>>")).isEqualTo(0)
+        assertThat(Day09Part2().solve("<{o\"i!a,<{i<a>")).isEqualTo(10)
     }
 
     @Test
     fun solvePart1() {
         val input = Utils.readLines("src/main/resources/Day09.txt")[0]
         println("Part 1: " + Day09Part1().solve(input))
-
     }
 
     @Test
     fun solvePart2() {
         val input = Utils.readLines("src/main/resources/Day09.txt")[0]
         println("Part 2: " + Day09Part2().solve(input))
-
     }
 
 }
