43078318256603d2463eedf541e859e9ab9bbe63
Andy Bell <andyrbell@gmail.com>
2017-12-13 22:52:10 +0000

    Day 13 part 1 - much simplified

--- src/main/kotlin/Day13Part1.kt
+++ src/main/kotlin/Day13Part1.kt
@@ -1,24 +1,14 @@
+import Utils.toPair
+
 class Day13Part1 {
     fun solve(input: List<String>): Int {
 
-        val firewallLayers = input.map { Utils.toPair(it) }.toMap().withDefault { _ -> -1 }
-        val maxDepth = firewallLayers.keys.max()!!
+        val firewallLayers = input.map { toPair(it) }.toMap()
 
-        fun packetPosition(time: Int): Int = time
-
-        var totalSeverity = 0
-        for (t in 0..maxDepth) {
-            val packetPosition = packetPosition(t)
-            val maxRange = firewallLayers.getValue(packetPosition)
-            if (maxRange != -1 && 0 == (t % ((maxRange - 1) * 2))) {
-
-                val severity = t * maxRange
-                println("t=$t; severity=$severity")
-                totalSeverity += severity
-            }
-        }
-
-        return totalSeverity
+        return firewallLayers.entries
+                .filter { (it.key % ((it.value - 1) * 2)) == 0 }
+                .map { it.key * it.value }
+                .sum()
     }
 
 }
