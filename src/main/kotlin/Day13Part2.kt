10f7db0fb3eee49d59d6bf83e9fc4740a6d01a63
Andy Bell <andyrbell@gmail.com>
2017-12-13 22:45:52 +0000

    Day 13 part 2 - much simplified

--- src/main/kotlin/Day13Part2.kt
+++ src/main/kotlin/Day13Part2.kt
@@ -1,26 +1,15 @@
+import Utils.toPair
+
 class Day13Part2 {
     fun solve(input: List<String>): Int {
 
-        val firewallLayers = input.map { Utils.toPair(it) }.toMap().withDefault { _ -> -1 }
-        val maxDepth = firewallLayers.keys.max()!!
-
-        fun packetPosition(time: Int): Int = time
+        val firewallLayers = input.map { toPair(it) }.toMap()
 
         fun caughtByScanner(delay: Int): Boolean {
-            var caught = false
-            for (t in 0..maxDepth) {
-                val packetPosition = packetPosition(t)
-                val maxRange = firewallLayers.getValue(packetPosition)
-                if (maxRange != -1 && 0 == ((t + delay) % ((maxRange - 1) * 2))) {
-
-                    caught = true
-                    break
-                }
-            }
-            return caught
+            return firewallLayers.entries
+                    .any { ((it.key + delay) % ((it.value - 1) * 2)) == 0 }
         }
 
         return (1..Int.MAX_VALUE).first { !caughtByScanner(it) }
     }
-
 }
