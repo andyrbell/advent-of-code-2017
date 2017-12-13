3a943adfaae953d85a455c431f62a0e42dbe550e
Andy Bell <andyrbell@gmail.com>
2017-12-13 22:35:35 +0000

    Day 13 part 2 - not pretty

--- src/test/kotlin/Day13Test.kt
+++ src/test/kotlin/Day13Test.kt
@@ -10,9 +10,22 @@ class Day13Test {
     }
 
     @Test
+    fun part2() {
+        val input = readLines("src/test/resources/Day13Test.txt")
+        Assertions.assertThat(Day13Part2().solve(input)).isEqualTo(10)
+    }
+
+    @Test
     fun solvePart1() {
         val input = readLines("src/main/resources/Day13.txt")
         println("Part 1: " + Day13Part1().solve(input))
+
+    }
+
+    @Test
+    fun solvePart2() {
+        val input = readLines("src/main/resources/Day13.txt")
+        println("Part 2: " + Day13Part2().solve(input))
 
     }
 }
