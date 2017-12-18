3c3a83942c4ed3db71aecca3405873819e16be1b
Andy Bell <andyrbell@gmail.com>
2017-12-18 14:49:17 +0000

    Day 16 part 2 back to the strings

--- src/main/kotlin/Day16Part2.kt
+++ src/main/kotlin/Day16Part2.kt
@@ -5,24 +5,24 @@ import Utils.spin
 import java.lang.Integer.parseInt
 
 class Day16Part2 {
-    fun solve(moves: List<String>, positions: CharArray): CharArray {
+    fun solve(moves: List<String>, positions: String): String {
         return moves.fold(positions, { acc, move ->
             when (move.take(1)) {
-                "s" -> spin(acc, parseInt(move.drop(1)))
+                "s" -> acc.spin(parseInt(move.drop(1)))
                 "x" -> {
                     val indexes = move.drop(1).split("/")
-                    exchange(acc, parseInt(indexes[0]), parseInt(indexes[1]))
+                    acc.exchange(parseInt(indexes[0]), parseInt(indexes[1]))
                 }
                 "p" -> {
                     val chars = move.drop(1).split("/").map { s -> s[0] }
-                    partner(acc, chars[0], chars[1])
+                    acc.partner(chars[0], chars[1])
                 }
                 else -> throw Exception("invalid move: $move")
             }
         })
     }
 
-    tailrec fun findCycleSize(moves: List<String>, positions: CharArray, original: CharArray, repetitions: Int = 1): Int {
+    tailrec fun findCycleSize(moves: List<String>, positions: String, original: String, repetitions: Int = 1): Int {
         if (repetitions % 10_000 == 0) println("looking for cycleSize: $repetitions")
 
         val result = solve(moves, positions)
@@ -33,9 +33,9 @@ class Day16Part2 {
         }
     }
 
-    fun solve(moves: List<String>, positions: CharArray, times: Int): CharArray {
+    fun solve(moves: List<String>, positions: String, times: Int): String {
 
-        val cycleSize = findCycleSize(moves, positions, positions.copyOf())
+        val cycleSize = findCycleSize(moves, positions, positions)
 
         println("cycleSize: $cycleSize")
 
--- src/test/kotlin/Day16Test.kt
+++ src/test/kotlin/Day16Test.kt
@@ -13,7 +13,7 @@ class Day16Test {
     @Test
     fun part2() {
         val input = splitCsv(readLines("src/test/resources/Day16Test.txt").get(0))
-        assertThat(Day16Part2().solve(input, "abcde".toCharArray(), 2)).isEqualTo("ceadb".toCharArray())
+        assertThat(Day16Part2().solve(input, "abcde", 2)).isEqualTo("ceadb")
     }
 
     @Test
@@ -25,6 +25,6 @@ class Day16Test {
     @Test
     fun solvePart2() {
         val input = splitCsv(readLines("src/main/resources/Day16.txt").get(0))
-        println("Part 2: " + Day16Part2().solve(input, "abcdefghijklmnop".toCharArray(), 1_000_000_000).toList())
+        println("Part 2: " + Day16Part2().solve(input, "abcdefghijklmnop", 1_000_000_000))
     }
 }
