aad9b5ce016616736033ff795cf73f979778eeb9
Andy Bell <andyrbell@gmail.com>
2017-12-19 23:24:56 +0000

    Day 19 parts 1 & 2

--- src/main/kotlin/Day19Part2.kt
+++ src/main/kotlin/Day19Part2.kt
@@ -1,15 +1,15 @@
 import Utils.Direction
 import Utils.Direction.*
 
-class Day19Part1 {
-    fun solve(input: List<String>): String {
+class Day19Part2 {
+    fun solve(input: List<String>): Int {
         val startX = input[0].indexOf('|')
         val initialPosition = Pair(startX, 0)
         val initialState = State(initialPosition, SOUTH)
         return recurse(initialState, input)
     }
 
-    tailrec fun recurse(state: State, input: List<String>): String {
+    tailrec fun recurse(state: State, input: List<String>): Int {
         val direction = state.direction
         val position = state.position
 
@@ -23,10 +23,10 @@ class Day19Part1 {
         val nextChar = input[nextInLine.second][nextInLine.first]
 
         return when {
-            nextChar.isLetter() -> recurse(State(nextInLine, direction, state.letters + nextChar), input)
-            nextChar.isWhitespace() -> state.letters
-            nextChar == '|' || nextChar == '-' -> recurse(State(nextInLine, direction, state.letters), input)
-            nextChar == '+' -> recurse(turn(State(nextInLine, direction, state.letters), input), input)
+            nextChar.isLetter() -> recurse(State(nextInLine, direction, state.letters + nextChar, state.steps + 1), input)
+            nextChar.isWhitespace() -> state.steps
+            nextChar == '|' || nextChar == '-' -> recurse(State(nextInLine, direction, state.letters, state.steps + 1), input)
+            nextChar == '+' -> recurse(turn(State(nextInLine, direction, state.letters, state.steps + 1), input), input)
             else -> throw IllegalArgumentException("invalid char: $nextChar")
         }
     }
@@ -37,20 +37,20 @@ class Day19Part1 {
 
         return when (direction) {
             NORTH, SOUTH -> listOf(
-                                State(Pair(position.first - 1, position.second), WEST, state.letters),
-                                State(Pair(position.first + 1, position.second), EAST, state.letters)
-                            )
+                    State(Pair(position.first - 1, position.second), WEST, state.letters),
+                    State(Pair(position.first + 1, position.second), EAST, state.letters)
+            )
             EAST, WEST -> listOf(
-                                State(Pair(position.first, position.second - 1), NORTH, state.letters),
-                                State(Pair(position.first, position.second + 1), SOUTH, state.letters)
-                            )
+                    State(Pair(position.first, position.second - 1), NORTH, state.letters),
+                    State(Pair(position.first, position.second + 1), SOUTH, state.letters)
+            )
         }
                 .filter { it.position.second in 0..(input.size - 1) }
                 .filter { it.position.first in 0..(input[it.position.second].length - 1) }
                 .first { !input[it.position.second][it.position.first].isWhitespace() }
-                .let { State(position, it.direction, it.letters) }
+                .let { State(position, it.direction, it.letters, state.steps) }
     }
 
-    data class State(val position: Pair<Int, Int>, val direction: Direction, val letters: String = "")
+    data class State(val position: Pair<Int, Int>, val direction: Direction, val letters: String = "", val steps: Int = 1)
 
 }
--- src/test/kotlin/Day19Test.kt
+++ src/test/kotlin/Day19Test.kt
@@ -10,8 +10,19 @@ class Day19Test {
     }
 
     @Test
+    fun part2() {
+        val input = readLines("src/test/resources/Day19Test.txt")
+        assertThat(Day19Part2().solve(input)).isEqualTo(38)
+    }
+
+    @Test
     fun solvePart1() {
         val input = readLines("src/main/resources/Day19.txt")
         println("Part 1: " + Day19Part1().solve(input))
     }
-}
+
+    @Test
+    fun solvePart2() {
+        val input = readLines("src/main/resources/Day19.txt")
+        println("Part 2: " + Day19Part2().solve(input))
+    }}
