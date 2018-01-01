6dd2a3c62e03b268de57ab2c87572c5b3c394070
Andy Bell <andyrbell@gmail.com>
2018-01-01 15:24:56 +0000

    Day 25 part 1 parse input

--- src/main/kotlin/Day25Part1.kt
+++ src/main/kotlin/Day25Part1.kt
@@ -1,60 +1,64 @@
 object Day25Part1 {
-    fun solve(steps: Int): Int {
+    fun solve(lines: List<String>): Int {
+        val beginInStateRegex = """Begin in state ([A-Z]).""".toRegex()
+        val beginInState = beginInStateRegex.find(lines[0])!!.groups[1]!!.value[0]
+        val stepsRegex = """Perform a diagnostic checksum after (\d+) steps.""".toRegex()
+        val steps = stepsRegex.find(lines[1])!!.groups[1]!!.value.toInt()
 
-        val state = move(State('A', 0, mutableMapOf()), steps)
-        return state.tape.values.count { it == 1 }
-    }
+        val rules = parseRules(lines)
 
-//    In state A:
-//    If the current value is 0:
-//    - Write the value 1.
-//    - Move one slot to the right.
-//    - Continue with state B.
-//    If the current value is 1:
-//    - Write the value 0.
-//    - Move one slot to the left.
-//    - Continue with state B.
-//
-//    In state B:
-//    If the current value is 0:
-//    - Write the value 1.
-//    - Move one slot to the left.
-//    - Continue with state A.
-//    If the current value is 1:
-//    - Write the value 1.
-//    - Move one slot to the right.
-//    - Continue with state A.
-
-    tailrec fun move(state: State, steps: Int): State {
-
-//        println("state: ${state.state} position: ${state.position} ${state.tape.toSortedMap()}")
-        if (steps == 0) {
-            return state
-        } else {
-            val commands = mapOf(
-                    'A' to Command(
-                            mapOf(0 to Instruction('B', 1, 1),
-                            1 to Instruction('B', 0, -1)
-                    )),
-                    'B' to Command(mapOf(
-                            0 to Instruction('A', 1, -1),
-                            1 to Instruction('A', 1, 1)
-                    )))
-
-            return move(commands[state.state]!!.execute(state), steps - 1)
+        val initialState = MachineState(beginInState, 0, mutableMapOf())
+        val machine = Machine(initialState, rules)
+        repeat(steps) {
+            machine.execute()
         }
+        return machine.checksum()
     }
 
-    data class State(val state: Char, val position: Int, val tape: MutableMap<Int, Int>)
+    private fun parseRules(lines: List<String>): Map<Char, Command> {
+        return lines.drop(3)
+                .chunked(10)
+                .associateBy(
+                        { it[0].takeLast(2).first() },
+                        { parseCommand(it) }
+                )
+    }
+
+    private fun parseCommand(lines: List<String>): Command {
+        return Command(mapOf(
+        0 to parseInstruction(lines.subList(2, 5)),
+        1 to parseInstruction(lines.subList(6, 9))
+        ))
+    }
+
+    private fun parseInstruction(lines: List<String>): Instruction {
+        val write = if (lines[0].contains("1")) 1 else 0
+        val move = if (lines[1].contains("right")) 1 else -1
+        val nextStateRegex = """.* Continue with state ([A-Z]).""".toRegex()
+        val nextState = nextStateRegex.find(lines[2])!!.groups[1]!!.value[0]
+        return Instruction(nextState, write, move)
+    }
+
+    data class MachineState(val state: Char, val position: Int, val tape: MutableMap<Int, Int>)
 
     data class Command(private val instructions: Map<Int, Instruction>) {
-        fun execute(state: State): State {
+        fun execute(state: MachineState): MachineState {
             val currentValue = state.tape.getOrDefault(state.position, 0)
             val instruction = instructions.getValue(currentValue)
             state.tape.put(state.position, instruction.write)
-            return State(instruction.nextState, state.position + instruction.move, state.tape)
+            return MachineState(instruction.nextState, state.position + instruction.move, state.tape)
         }
     }
 
     data class Instruction(val nextState: Char, val write: Int, val move: Int)
+
+    data class Machine(private var machineState: MachineState, val rules: Map<Char, Command>) {
+        private fun nextCommand(): Command = rules.getValue(machineState.state)
+
+        fun execute() {
+            machineState = nextCommand().execute(machineState)
+        }
+
+        fun checksum() = machineState.tape.values.count { it == 1 }
+    }
 }
